59354cb812f6a63a6e49ca0912c7e90238833c54
nishanthspk <spknishanth2004@gmail.com>
2024-09-16 12:40:23 +0530

    Login-Del

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Login from './components/accounts/login.jsx';
+
 
 function App() {
   return (
     <div className="">
-     <Login />
+    
     </div>
   );
 }
