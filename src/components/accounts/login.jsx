8e38aecb9ebddac1753733b8c55e00808cea98ef
nishanthspk <spknishanth2004@gmail.com>
2024-09-16 10:26:02 +0530

    box component

--- src/components/accounts/login.jsx
+++ src/components/accounts/login.jsx
@@ -8,13 +8,7 @@ const Login = () => {
 
 
   return (
-  <Box>
-  <img src={imageURL} alt="" />
-  <TextField id="standard-basic" label="Standard" variant="standard" />
-  <TextField id="standard-basic" label="Standard" variant="standard" />
-  <Button variant="contained">Login</Button>
-  <Button>Create An Account</Button>
-  </Box>
+ 
   )
 }
 export default Login;
