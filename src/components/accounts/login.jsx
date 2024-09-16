e76ba1247a67884daef3dd720e854867c81d3841
nishanthspk <spknishanth2004@gmail.com>
2024-09-16 10:25:44 +0530

    image

--- src/components/accounts/login.jsx
+++ src/components/accounts/login.jsx
@@ -6,7 +6,7 @@ import {  Box,TextField,Button,styled } from '@mui/material';
 
 const Login = () => {
 
-const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+
   return (
   <Box>
   <img src={imageURL} alt="" />
