d302bcbb1df62711f1878ae8bb9ac450c35be32b
nishanthspk <spknishanth2004@gmail.com>
2024-09-16 14:20:02 +0530

    useeffect

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Post from "../Post";
+import {useEffect, useState} from "react";
 
 export default function IndexPage() {
+  const [posts,setPosts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/post').then(response => {
+      response.json().then(posts => {
+        setPosts(posts);
+      });
+    });
+  }, []);
   return (
-    <div>IndexPage</div>
-  )
+    <>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+      ))}
+    </>
+  );
 }
