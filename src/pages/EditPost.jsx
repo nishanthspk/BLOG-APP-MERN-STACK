5406193439bbe2b6f27c767e820312773685c8ff
nishanthspk <spknishanth2004@gmail.com>
2024-09-19 10:45:58 +0530

    registerpage

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch('http://localhost:3000/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:4000/post').then(response => {
+    fetch('http://localhost:3000/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
