445138808b00695117f3e7e9d3ec85a6b3dd179e
nishanthspk <spknishanth2004@gmail.com>
2024-09-19 10:17:50 +0530

    post page

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -10,7 +10,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
+    fetch(`http://localhost:3000/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
